d85745cc3bad1331918ac25993ae5bdb570d2044
Bernhard <kadmus@nexgo.de>
2024-12-21 21:09:13 +0100

    Highest new and climbing, last ts compared to entire week.

--- evalkeys.py
+++ evalkeys.py
@@ -1,6 +1,14 @@
 from datetime import datetime
 from datetime import timedelta
 import sqlite3
+import re
+
+def contains_special_char_word(text):
+  words = text.split()
+  for word in words:
+    if re.match(r'^[^a-zA-Z0-9]+$', word):
+      return True
+  return False
 
 def weekyear(timestamp):
     iso_week_number = timestamp.isocalendar().week
@@ -23,6 +31,12 @@ def main():
     lastweek_timestamp = current_timestamp - timedelta(days=7)
     lastweek=weekyear(lastweek_timestamp)
 
+    #cursor.execute("DELETE from keywords where ts='2024-12-21 17:48:27'")
+    #conn.commit()
+
+    #cursor.execute("DELETE from keywords where ts='2024-12-21 17:50:13'")
+    #conn.commit()
+
     cursor.execute(f'SELECT COUNT(*) FROM keywords')
     count = cursor.fetchone()[0]  # Fetch the result
     print(f'The number of records in the table "keywords" is: {count}')
@@ -31,21 +45,47 @@ def main():
     tsthis = cursor.fetchone()[0]  # Fetch the result
     print(f'The number of timestamps in the table "keywords" for this week is: {tsthis}')
 
-    cursor.execute("select phrase, s_score from (select phrase, sum(score) s_score from keywords where week=? group by phrase) x order by s_score desc limit 50",(thisweek,))
+    cursor.execute("SELECT max(ts) mts FROM keywords where week=?",(thisweek,))
+    latestts = cursor.fetchone()[0]  # Fetch the result
+    print(f'The latest timestamp in the table "keywords" for this week is: {latestts}')
+
+    cursor.execute('''
+        create temporary table weekstat as
+        select phrase, score from (
+            select phrase, sum(score)/? score
+            from keywords
+            where week=?
+            and ts!=?
+            group by phrase) x''',(tsthis,thisweek,latestts))
+    conn.commit()
+
+    print("find words with only special characters")
+    cursor.execute('''
+        SELECT ts, week, phrase, score FROM keywords limit 2000''')
+    for row in cursor:
+        if contains_special_char_word(row[2]):
+            print(row, re.sub(' [^a-zA-Z0-9 ]+ ', ' ', ' '+row[2])+' ')
+
+    print("highest new phrases")
+    cursor.execute('''
+        select k.score, k.phrase
+        from keywords k left outer join weekstat w
+        on k.phrase = w.phrase
+        where w.phrase is NULL
+        and k.ts=?
+        order by k.score desc limit 20''',(latestts,))
     for row in cursor:
         print(row)
 
-    cursor.execute("update keywords set ts='2024-12-17 22:19:02' where ts='2024-12-17 22:19:02.550665'")
-    conn.commit()
-
-    cursor.execute("update keywords set ts='2024-12-17 19:59:40' where ts='2024-12-17 19:59:40.316094'")
-    conn.commit()
-
-    cursor.execute("update keywords set ts='2024-12-18 17:57:26' where ts='2024-12-18 17:5726'")
-    conn.commit()
-
-    cursor.execute("update keywords set ts='2024-12-18 20:23:53' where ts='2024-12-18 20:2353'")
-    conn.commit()
+    print("highest climbing phrases")
+    cursor.execute('''
+        select k.score, k.phrase
+        from keywords k, weekstat w
+        where k.phrase = w.phrase
+        and k.ts=?
+        order by k.score/w.score desc limit 20''',(latestts,))
+    for row in cursor:
+        print(row)
 
     cursor.execute("select week, ts, count(*) from keywords group by ts, week")
     for row in cursor:
